6686c69a07d0d18c9fba21258ea11fb2b9f56c06
Nelson1499 <avalosnelson9919@gmail.com>
2024-04-15 01:39:35 -0300

    mejoras en mensajes birdsocial

--- src/components/list/Listusers.tsx
+++ src/components/list/Listusers.tsx
@@ -28,61 +28,62 @@ export default function Listusers ({
   }
   return (
     <>
-      <div className="fixed inset-0 bg-black bg-opacity-75 z-20"></div>
-      <div className="z-20 w-full h-full object-contain tablet:max-h-[600px] tablet:max-w-[480px] tablet:m-auto fixed tablet:top-5 bg-white dark:bg-black tablet:rounded">
-        <header className="border-b-2 border-gray-700">
-          <div className="p-2 space-y-2">
-            <div className="flex space-x-2 ">
-              <ClearIcon
-                className="cursor-pointer"
-                onClick={() => {
-                  setshowList(false)
-                }}
-              />
-              <h1 className="text-xl font-extrabold ">Nuevo Mensaje</h1>
-            </div>
-            <label className="flex space-x-2">
-              <SearchIcon />
-              <input
-                type="search"
-                className="bg-transparent outline-none w-full flex"
-                placeholder="Buscar personas"
-                onChange={showusers}
-              />
-            </label>
-          </div>
-        </header>
-        <ul className="w-full flex justify-center items-center">
-          {character !== ""
-            ? (
-            <Fragment>
-              {users?.map((user, i) => (
-                <li
-                  key={i}
+      <div className="fixed w-full h-full object-contain flex inset-0 bg-black bg-opacity-75 z-20 justify-center items-center">
+        <div className="z-20 w-full h-full tablet:max-h-[600px] tablet:max-w-[480px] tablet:m-auto absolute m-auto bg-white dark:bg-black tablet:rounded">
+          <header className="border-b-2 border-gray-700">
+            <div className="p-2 space-y-2">
+              <div className="flex space-x-2 ">
+                <ClearIcon
+                  className="cursor-pointer"
                   onClick={() => {
-                    navigation(user.id)
+                    setshowList(false)
                   }}
-                  className="flex p-2 hover:bg-black hover:bg-opacity-30 cursor-pointer"
-                >
-                  <Image
-                    className="rounded-full"
-                    src={user.avatar_url}
-                    height={50}
-                    width={50}
-                    alt="profile"
-                  />
-                  <div className="px-1">
-                    <h1>{user.name}</h1>
-                    <h2 className="text-gray-700">@{user.username}</h2>
-                  </div>
-                </li>
-              ))}
-            </Fragment>
-              )
-            : (
-            <li className="text-center pt-10 font-semibold text-xl">Intenta buscar personas.</li>
-              )}
-        </ul>
+                />
+                <h1 className="text-xl font-extrabold ">Nuevo Mensaje</h1>
+              </div>
+              <label className="flex space-x-2">
+                <SearchIcon />
+                <input
+                  type="search"
+                  className="bg-transparent outline-none w-full flex"
+                  placeholder="Buscar personas"
+                  onChange={showusers}
+                />
+              </label>
+            </div>
+          </header>
+          <ul className="w-full justify-center items-center">
+            {character !== ""
+              ? (
+              <Fragment>
+                {users?.map((user, i) => (
+                  <li
+                    key={i}
+                    onClick={() => {
+                      navigation(user.id)
+                    }}
+                    className="flex p-2 hover:bg-black hover:bg-opacity-30 cursor-pointer"
+                  >
+                    <Image
+                      className="rounded-full"
+                      src={user.avatar_url}
+                      height={50}
+                      width={50}
+                      alt="profile"
+                    />
+                    <div className="px-1">
+                      <h1>{user.name}</h1>
+                      <h2 className="text-gray-700">@{user.username}</h2>
+                    </div>
+                  </li>
+                ))}
+              </Fragment>
+                )
+              : (
+              <li className="text-center pt-10 font-semibold text-xl">Intenta buscar personas.</li>
+                )}
+          </ul>
+        </div>
       </div>
     </>
   )
